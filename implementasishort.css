aa6c2bb169cda9ce554c055cd337135a62a8f077
EA Nugroho <69884456+JourneyEAN@users.noreply.github.com>
2024-09-10 13:49:54 +0700

    Create implementasishort.css

---
+++ implementasishort.css
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main ()
+{
+
+cout << "##  Tugas 14 Implementasi algoritma short ##" << endl;
+cout << "=========================================" << endl;
+cout << endl;
+cout << "nama          : Eko Ari Nugroho"<< endl;
+cout << "nim           : 5520122022"<< endl;
+cout << endl;
+cout << "=========================================" << endl;
+cout << endl;
+cout << "Dalam Sistem Secara bersamaan mahasiswa menginputkan tahun lahir sehingga menjadi tak beratur  : \n"<< endl;
+cout << "sebagai berikut :\n"<< endl;
+cout << "1992, 1993, 2000, 1997, 2003, 1994, 2005, 1996 \n"<< endl;
+   
+      
+    int array[] = { 1992, 1993, 2000, 1997, 2003, 1994, 2005, 1996 };
+    int n = sizeof(array) / sizeof(array[0]);
+  
+    // urutan tahun kelahiran mahasiswa dari usia yang tua hingga muda 
+  
+    sort(array + 2, array + n);
+  
+    cout << "Tahun kelahiran mahasiswa semester 1 tahun 2023 Setelah di Shorting  : \n"<< endl;
+    for (int i = 0; i < n; ++i)
+        cout << array[i] << " ";
+  
+    return 0;
+}
