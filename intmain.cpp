ac6a418ff1cd85c0e2ef27177dce65b49eb4aa2f
EA Nugroho <69884456+JourneyEAN@users.noreply.github.com>
2024-09-10 13:58:57 +0700

    Create intmain.cpp

---
+++ intmain.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	cout << "nama          : eko ari nugroho"<< endl;
+        cout << "nim           : 081383796300"<< endl; 
+        cout << "jenis kelamin : L"<< endl;
+        return 0;
+}
